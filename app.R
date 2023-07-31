a74f4fc5274c1525377f167dac332e8caa56ccd0
Rachel Shaw <117684828+rachel-shaw@users.noreply.github.com>
2023-07-30 22:06:06 -0600

    App: Year Slider & Layout Updated
    added year slider connectivity, credit banner at bottom, updated layout

--- app.R
+++ app.R
@@ -31,52 +31,99 @@ ui = navbarPage(
   theme = shinytheme("flatly"),
   tabPanel(
     "Main",
+    
     # App title ----
     titlePanel(div(
       windowTitle = "",
       img(src = "titlebanner_blue.jpg", width = "100%", class = "bg"),
     )),
   
-  ##########################################
-  ####  Panel: Summary                  ####
-  ##########################################
-  tabsetPanel(
-    type = "tabs",
-    tabPanel(
-      "Summary",
-      
-      sidebarLayout(
+    ##########################################
+    ####  Panel: Summary                  ####
+    ##########################################
+    tabsetPanel(
+      type = "tabs",
+      tabPanel(
+        "Summary",
         
-        sidebarPanel(
-          #add drop down section to select metric for units sold
-          selectInput(inputId = "units_sold",
-                      label = "Units Sold",
-                      list("Total", "Average")),
-          textOutput("selected_unitssold"),
-          sliderInput("YearRange",
-                      label = "Year Range",
-                      min = 2000,
-                      max = 2020,
-                      value = c(2000, 2020)),
+        fluidPage(
+        
+        fluidRow(
           
-          selectInput(inputId = "production_cost", 
-                      label = "Production Cost", 
-                      list("Total", "Average")),
-          textOutput("selected_productioncost")
-        ),
-        
-        mainPanel(
-          plotOutput("units_sold_plot"),
-          tableOutput("units_sold_table"),
+          ##UNITS SOLD PANEL
+          column(12, align = "center", 
+          h2(textOutput("welcome")),
+          tags$br(),
+          htmlOutput("welcome_details"),
           
-          plotOutput("productioncost_plot"),
-          tableOutput("productioncost_table"),
-        )
-        
-        
-      )
-    ),
-    
+          tags$hr(),
+          ),
+          
+          sidebarLayout(
+              sidebarPanel(
+                #add drop down section to select metric for units sold
+                h3(textOutput("units_sold_title")),
+                textOutput("units_sold_instructions"),
+                tags$br(),
+                
+                selectInput(inputId = "units_sold",
+                            label = "Select Metric",
+                            list("Total", "Average")),
+                sliderInput("UnitsSold_YearRange",
+                            label = "Year Range",
+                            min = 2000,
+                            max = 2020,
+                            value = c(2000,2020)),
+                tags$br(),
+                tags$br(),
+                tags$br(),
+                tags$br(),
+                tags$br(),
+                tags$br(),
+              ),
+              
+              mainPanel(
+                tags$br(),
+                fluidRow(
+                  column(8, plotOutput("units_sold_plot")),
+                  column(4, tableOutput("units_sold_table")))
+                )
+              ), #end of sidebar layout
+              
+              tags$hr(),
+          
+          ##PRODUCTION COST PANEL    
+          sidebarLayout(
+              sidebarPanel(
+              #add drop down section to select metric for production costs
+                h3(textOutput("production_cost_title")),
+                textOutput("production_cost_instructions"),
+              tags$br(),
+              selectInput(inputId = "production_cost", 
+                            label = "Select Metric", 
+                            list("Total", "Average")),
+                br(),
+              ), #end of sidebarPanel
+              
+              mainPanel(
+                fluidRow(
+                  column(8, plotOutput("productioncost_plot")),
+                  column(4, tableOutput("productioncost_table"))),
+                
+                tags$br(),
+                tags$br(),)  
+             ),#end of sidebarLayout
+                
+              tags$hr(),
+          
+          column(12, align = "center", 
+              textOutput("tabs_above_instructions")),
+              
+              tags$br(),
+              tags$br(),
+              tags$br(),
+              tags$br()
+          ))),
     
     
     ##########################################
@@ -84,54 +131,49 @@ ui = navbarPage(
     ##########################################
     tabPanel(
       "Platform",
-      
+        
     ),
-    
-    
+      
+      
     ##########################################
     ####  Panel: Genre                    ####
-    ##########################################
-    
+    ##########################################     
     tabPanel(
       "Genre"
-    )
-  )
-),
-
-    ################################################
-    #### Panel: Documentation                   ####
-    ################################################
+        
+    ),
+      
     
-    tabPanel("Documentation",
-             fluidPage(htmlOutput("doc"))),
+
+      
+      )),
+
+  # ------------------
+  # Documentation Section
+  # ------------------
+
     
-    ################################################
-    #### Panel: About                           ####
-    ################################################
-    tabPanel("About",
-             fluidPage(htmlOutput("abo")))
-)
+  tabPanel("Documentation",
+           fluidPage(htmlOutput("doc"))),
+
+  # ------------------
+  # About Section
+  # ------------------
+  
+  tabPanel("About",
+           fluidPage(htmlOutput("abo"))),
+  
+  
+  # credit banner----
+  footer = (div(
+    windowTitle = "",
+    img(src = "Dashboard_Credit_Banner.jpg", width = "100%", class = "bg"),
+  )),
+  
+  
+) #end of navbarPage
 
 
-# ui = fluidPage(
-#   h1("Video Game Industry Dashboard"),
-#   
-#   #add drop down section to select metric for units sold
-#   selectInput(inputId = "units_sold", 
-#               label = "Units Sold", 
-#               list("Total", "Average")),
-#   textOutput("selected_unitssold"),
-#   plotOutput("units_sold_plot"),
-#   tableOutput("units_sold_table"),
-#   
-#   #add drop down section to select metric for production cost
-#   selectInput(inputId = "production_cost", 
-#               label = "Production Cost", 
-#               list("Total", "Average")),
-#   textOutput("selected_productioncost"),
-#   plotOutput("productioncost_plot"),
-#   
-# )
 
 
 
@@ -145,17 +187,31 @@ ui = navbarPage(
 ####   Shiny Server                   ####
 ##########################################
 #where dqtq is manipulated and visualizations are prepared
-server = function(input, output) {
+server = function(input, output, session) {
   
   #import data
   videogamesales_db <- read_excel("video_game_sales_cleaned.xlsx")
   
   ##UNITS SOLD SECTION
-  #output text for selected units sold metric
-  output$selected_unitssold <- renderText({
-      paste("Showing results for", input$units_sold, "Video Game Units Sold Across Regions")
+  #output text for side panel
+  output$welcome <- renderText({
+    paste("Welcome to the Video Game Industry Dashboard!")
+  })
+  
+  output$welcome_details <- renderUI({
+    HTML(paste("This summary page provides an overview of video game sales and production costs from 2000 to 2020. If you'd like an in-depth analysis, use the tabs above to view game sales by platform and genre.", "Happy analyzing!", sep = "<br>"))
+  })
+  
+  output$tabs_above_instructions <- renderText({
+      paste("Interested in learning more about how this dashboard was created? Use the `Documentation` and `About` tabs at the top of the page to view the code used in RShiny and learn about the analyst.")
     })
 
+  output$units_sold_title <- renderText("Number of Units Sold")
+  output$units_sold_instructions <- renderText({paste("To view results of the number of game units old, please select a metric from the drop-down menu. The range of years populated in the graph can be adjusted through the slider.")})
+  
+  output$production_cost_title <- renderText("Production Cost")
+  output$production_cost_instructions <- renderText({paste("To view data on production costs each year, please select a metric from the drop-down menu.")})
+  
   #data
   summary_sales_db <- videogamesales_db %>%
     group_by(release_year) %>%
@@ -188,13 +244,26 @@ server = function(input, output) {
       #return correct dataset based on selection
       unitssold_datasetInput <- reactive({
         req(input$units_sold)
-        df <- units_sales_db %>% filter(Metric %in% input$units_sold)
+        df <- units_sales_db %>% filter(Metric %in% input$units_sold) %>% filter(release_year >= input$UnitsSold_YearRange[1] & release_year <= input$UnitsSold_YearRange[2])
       }) 
       
   #plot
   output$units_sold_plot <- renderPlot({
-    g <- ggplot(unitssold_datasetInput(), aes(y = Sales, x = release_year, color= factor(Region))) 
-    g + geom_line() + labs(x="Year",y="Millions of Copies Sold")
+    g <- ggplot(unitssold_datasetInput(), 
+                aes(y = Sales, 
+                    x = release_year, 
+                    color= factor(Region))) 
+    g + geom_line(size = 1) + 
+      labs(x="Year",
+           y="Millions of Copies Sold",
+           color = "Regions") +
+      ggtitle(paste(input$units_sold, "Video Game Units Sold Across Regions")) +
+      theme_bw() + 
+      theme(plot.title = element_text(size=16, face="bold", hjust = 0.5),
+            legend.position = "right",
+            axis.title.x = element_text(size = 14, vjust = 1.3),
+            axis.title.y = element_text(size = 14, vjust = 1.3),
+            axis.text = element_text(size = 12))
   })
   
   #table
@@ -242,8 +311,15 @@ server = function(input, output) {
   #plot production costs total
   output$productioncost_plot <- renderPlot({
     g <- ggplot(productioncost_datasetInput(), aes(x=release_year, y=productioncost))
-    g + geom_line() + labs(x="Year",y="Millions of Dollars")
-  })
+    g + geom_line() + 
+      labs(x="Year",
+           y="Millions of Dollars") +
+    theme_bw() + 
+      theme(legend.position = "right",
+            axis.title.x = element_text(size = 14, vjust = 1.3),
+            axis.title.y = element_text(size = 14, vjust = 1.3),
+            axis.text = element_text(size = 12))
+    })
   
   #table
   top_prodcost_db <- total_productioncost_db %>%
